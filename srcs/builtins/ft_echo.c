82fb47c095f0377db85edeab3f2719a0e04e7562
Crealex <Alexandre.tomasi80@gmail.com>
2025-01-14 22:46:00 +0100

    progress ft_echo but a fresh restart is maybe better...

--- srcs/builtins/ft_echo.c
+++ srcs/builtins/ft_echo.c
@@ -3,16 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   ft_echo.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 14:42:41 by atomasi           #+#    #+#             */
-/*   Updated: 2025/01/14 18:25:17 by atomasi          ###   ########.fr       */
+/*   Updated: 2025/01/14 22:44:21 by alexandre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-char *join_prompt(char **prompt, int i)
+// tout revoir et imprimer caractere par caractere plus que une string par string
+
+static char *join_prompt(char **prompt, int i)
 {
 	char *result;
 	char *temp;
@@ -38,19 +40,72 @@ char *join_prompt(char **prompt, int i)
 	return (result);
 }
 
-/* static void	print_args(char **prompt, int s)
+static char *handle_dollar(char **prompt, int i) // a revoir pour opti, les free etc...
+{
+	char *temp;
+	char **tab;
+	char *env;
+	char *result = "gestion du dollar";
+	int j;
+
+	j = 0;
+	while (prompt[i])
+	{
+		if (prompt[i][j] == '$')
+		{
+			tab = ft_split(prompt[i], '$');
+			temp = ft_strdup(tab[0]);
+			env = ft_strdup(tab[1]);
+			tab = ft_split(tab[1], ' ');
+			free(env);
+			free(temp);
+			env = ft_strdup(tab[0]);
+		}
+		if (!prompt[i][j])
+			i++;
+	}
+	result = getenv(tab[0]);
+	return (result);
+}
+
+static char *handle_quote(char **prompt, int i, char c) // a tester partout enfate et pas uniquement avec une quote au debut
+{
+	char *result;
+	char *line;
+	int ires;
+	int quote;
+
+	ires = 0;
+	quote = 0;
+	line = join_prompt(prompt, i);
+	result = malloc(sizeof(char) * ft_strlen(line));
+	while (line[ires + quote])
+	{
+		if (line[ires + quote] == c)
+			quote++;
+		result[ires] = line[ires + quote];
+		ires++;
+	}
+	result[ires] = '\0';
+	if (quote % 2 != 0)
+		printf("nombre impaire de quote\n"); // trouver comment ouvrir l'ecoute
+	return (result);
+}
+
+static void	print_args(char **prompt, int s)
 {
 	char *result;
 
 	if (prompt[s][0] == '\'')
-		result = handle_quote();
+		result = handle_quote(prompt, s, '\'');
 	else if (prompt[s][0] == '\"')
-		result = handle_quote();
-	else if (prompt[s][0] == '$')
-		result = handle_dollar();
-	else
-		result = join_prompt(prompt, s);
-} */
+		result = handle_quote(prompt, s, '\"');
+	//else if (prompt[s][0] == '$')
+		result = handle_dollar(prompt, s);
+	//else
+		//result = join_prompt(prompt, s);
+	ft_putstr_fd(result, 1);
+}
 
 void	ft_echo(char **prompt)
 {
@@ -58,25 +113,25 @@ void	ft_echo(char **prompt)
 	//2. gerer les ""
 	//3. gerer les $
 	//4. gerer l'option -n
+	//5. gerer les cas speciaux
 	if (!prompt[1])
 	{
-		ft_putstr_fd("\n", 0);
+		ft_putstr_fd("\n", 1);
 		return ;
 	}
 	if (prompt[1][0] == '-' && prompt[1][1] == 'n')
 	{
 		if (prompt[2])
-			ft_putstr_fd(prompt[2], 0);
+			print_args(prompt, 2);
 		else
-			ft_putstr_fd("\n", 0);
+			ft_putstr_fd("\n", 1);
 		return ;
 	}
-	//5. gerer les cas speciaux
-	ft_putstr_fd(prompt[1], 0);
-	ft_putstr_fd("\n", 0);
+	print_args(prompt, 1);
+	ft_putstr_fd("\n", 1);
 }
 
-int main(int argc, char **argv)
+/* int main(int argc, char **argv)
 {
 	char *result;
 
@@ -84,4 +139,4 @@ int main(int argc, char **argv)
 	result = join_prompt(argv, 1);
 	printf("result : %s\n", result);
 	free(result);
-}
+} */

bedb04568d42bf1ab7844bdac509e0060fb2be9a
neoo-n <dana.vauthey@gmail.com>
2025-03-07 11:22:21 +0100

    merge

--- srcs/parsing/get_out_fd.c
+++ srcs/parsing/get_out_fd.c
@@ -6,7 +6,7 @@
 /*   By: dvauthey <dvauthey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 16:08:33 by dvauthey          #+#    #+#             */
-/*   Updated: 2025/03/05 14:32:10 by dvauthey         ###   ########.fr       */
+/*   Updated: 2025/03/07 11:15:33 by dvauthey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ static int	open_fd(char *str, int fd_arg, int *len, int is_double)
 	char	*str_cut;
 	int		s;
 
+	fprintf(stderr, "here\n");
 	if (fd_arg > 2)
 		close(fd_arg);
 	s = len[0] + 1;
@@ -55,6 +56,7 @@ static int	open_fd(char *str, int fd_arg, int *len, int is_double)
 	while (str[s] && ft_isspace(str[s]))
 		s++;
 	str_cut = filename(str, s);
+	fprintf(stderr, "str cut : %s\n", str_cut);
 	if (!str_cut)
 		return (-1);
 	if (!is_double)
